c237fb2531b6ed7509704d8748b1a6ced2f430af
Bockstael Kévin <kevin.bockstael@hotmail.com>
2022-11-17 09:47:27 +0100

    add Warrior

---
+++ Warrior.py
@@ -0,0 +1,9 @@
+import random;
+
+class Warrior:
+
+    def __init__(self):
+        self.__degat = random.choice(range(3,5))
+        self.__chance = 5
+        self.__prix = 10
+        self.__type = "warrior"
