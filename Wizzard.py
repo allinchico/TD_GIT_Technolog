f44e8aa9d6d8f56c69b194be8b163245f905aebe
bkebir <bkebir@iut.univ-paris8.fr>
2022-11-17 10:35:03 +0100

    class wizzard

---
+++ Wizzard.py
@@ -0,0 +1,16 @@
+import random
+
+class Wizzard:
+
+    def __init__(self):
+        self.__degat = random.choice(range(2,4))
+        self.__chance = 20
+        self.__fuite = 10
+        self.__prix = 15
+        self.__unite = "wizzard"
+
+
+
+
+
+      
